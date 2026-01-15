fe496bc50c9105cfcdb321eca6c2f9d6007f3d8d
Johnpaul Ugwulo <jugwulo@infinion.co>
2026-01-15 09:25:42 +0100

    add application files

---
+++ app/production/app.js
@@ -0,0 +1,17 @@
+// this is just for this test task
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/health', (req, res) => res.status(200).send('App is running OK'));
+app.get('/status', (req, res) => res.status(200).json({ status: 'UP', timestamp: new Date() }));
+app.post('/process', (req, res) => {
+    const data = req.body;
+    res.status(202).json({ message: 'Data received', data });
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
+
+module.exports = app; // For testing
